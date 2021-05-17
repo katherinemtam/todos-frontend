e6de9960d1e807ea4dc14d51e5a3134cbf9edf5b
Matthew Saber <mattsaber21@yahoo.com>
2021-05-17 16:05:04 -0700

    fixed quote in blurb

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
     return (
       <div className="Home">
         <h1>The Bestest Tado App</h1>
-        <h2>Yes, we know how to spell "todo", but you'll be so amazed by what you'll accomplish you'll be shouting "TADA" down the isles of your local supermarket.</h2>
+        <h2>Yes, we know how to spell "todo", but you'll be so amazed by what you accomplish you'll be shouting "TADA" down the isles of your local supermarket.</h2>
         <h3>The proprietors of this app take no responsibility for your outlandish exclaims of joy resulting from completing your todos.</h3>
 
         <Link to='/resources'>See the List</Link>
