ce79cd51193063f148cd6aed25d4d729740ca56a
Katherine Tam <74214477+katherinemtam@users.noreply.github.com>
2021-05-18 15:45:44 -0700

    Revert "Matt"

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,20 +1,18 @@
 import { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
 
-  render() {
+  render() { 
     return (
       <header className="Header">
-        <h1>The Bestest Tado App</h1>
-        <h2>Your excuses end here.</h2>
 
-        <NavLink to="/todos">Tado List</NavLink>
+        <h1>React App</h1>
+        
       </header>
     );
   }
 
 }
-
+ 
 export default Header;
--- src/home/Home.js
+++ src/home/Home.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default class Home extends Component {
-
+  
   render() {
     return (
       <div className="Home">
-        <h1>The Bestest Tado App</h1>
-        <h2>Yes, we know how to spell "todo", but you'll be so amazed by what you accomplish you'll be shouting "TADA" down the isles of your local supermarket.</h2>
-        <h3>The proprietors of this app take no responsibility for your outlandish exclaims of joy resulting from completing your todos.</h3>
+        <h2>Home Page</h2>
 
         <Link to='/resources'>See the List</Link>
       </div>
